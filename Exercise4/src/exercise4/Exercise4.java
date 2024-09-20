a60a27acd29c4e65d719de082e489b14d2fa6ea2
ruben.carrasco <ruben.carrasco@106-05.info2>
2024-09-20 14:05:33 +0200

    Exercise4 terminado

---
+++ Exercise4/src/exercise4/Exercise4.java
@@ -0,0 +1,12 @@
+package exercise4;
+
+public class Exercise4 {
+
+	public static void main(String[] args) {
+		System.out.println("I want this program to compile");
+		// Los errores eran la s e system no estaba en mayúsculas
+		// la cadena no estaba entre comillas y faltaba el punto y coma final
+
+	}
+
+}
